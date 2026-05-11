3bced7b59b4ce4e52d3d3ce755af7bb1a606eb14
pkonarski <pkaotn99@gmail.com>
2026-05-11 21:38:58 +0200

    ADD basic project skele

---
+++ src/test/java/com/pk/coupon/TestCouponApplication.java
@@ -0,0 +1,11 @@
+package com.pk.coupon;
+
+import org.springframework.boot.SpringApplication;
+
+public class TestCouponApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.from(CouponApplication::main).with(TestcontainersConfiguration.class).run(args);
+    }
+
+}
